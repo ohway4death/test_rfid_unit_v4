68774460907d1525f1692ed7ad92903c174baf93
ohway4death <ohway.pretz411@gmail.com>
2024-02-15 01:03:55 +0900

    gyro test

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 //
 //						Jun/8/2022
 // ---------------------------------------------------------------
+#define M5STACK_MPU6886
+
 #include <M5Core2.h>
 #include <driver/i2s.h>
 #include "MFRC522_I2C.h"
@@ -22,9 +24,10 @@
 #define queen "1d fd 6e 58"
 #define king "1d e1 27 55"
 
-#define WAIT    1
-#define SCAN    2
-#define RESULT  3
+#define WAIT 1
+#define SCAN 2
+#define RESULT 3
+#define TEST 4
 
 static LGFX lcd; // LGFXのインスタンスを作成。
 
@@ -39,12 +42,13 @@ LGFX_Sprite spriteWord4(&spriteBase); // スプライトを使う場合はLGFX_S
 
 MFRC522_I2C mfrc522(0x28, -1); // Create MFRC522 instance.
 
-
 void uid_display_proc();
 void display_trump();
 void wait();
 void scan();
 void result();
+void zeroReset();
+void check_acc();
 
 // ---------------------------------------------------------------
 void setup()
@@ -86,6 +90,9 @@ void setup()
   Wire.begin();
   mfrc522.PCD_Init();
 
+  M5.IMU.Init();                     // 6軸センサ初期化
+  M5.IMU.SetAccelFsr(M5.IMU.AFS_2G); // 加速度センサースケール初期値 ±2G(2,4,8,16)
+  zeroReset();                       // 0リセット
 }
 
 // ---------------------------------------------------------------
@@ -100,16 +107,19 @@ int state = WAIT;
 void loop()
 {
 
-  if (state == WAIT){
+  if (state == WAIT)
+  {
     wait();
-  }else if (state == SCAN)
+  }
+  else if (state == SCAN)
   {
     scan();
-  }else if (state == RESULT)
+  }
+  else if (state == RESULT || state == TEST)
   {
     result();
   }
-    delay(200);
+  delay(200);
 }
 
 // ---------------------------------------------------------------
@@ -162,7 +172,8 @@ void display_trump()
 }
 // ---------------------------------------------------------------
 
-void wait(){
+void wait()
+{
 
   spriteBase.fillScreen(BLACK); // 画面の塗りつぶし
   spriteWord1.pushRotateZoom(280, 120, 90, 1, 1);
@@ -174,20 +185,92 @@ void wait(){
   if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial())
   {
     return;
-  }else{
+  }
+  else
+  {
     state = SCAN;
   }
 }
 
-void scan(){
+void scan()
+{
   display_trump();
   state = RESULT;
 }
 
-void result(){
+
+float baseAccX, baseAccY, baseAccZ = 0; // 基準加速度格納用
+float accX, accY, accZ;                 // 加速度格納用
+float diffAccX, diffAccY, diffAccZ = 0;    // 現在値格納用
+
+float maxAccX, maxAccY, maxAccZ = 0;
+
+void result()
+{
   M5.update();
   if (M5.BtnA.isPressed())
   {
     state = WAIT;
   }
+  else if (M5.BtnC.isPressed())
+  {
+    state = TEST;
+    zeroReset();
+  }
+
+  if (state == TEST){
+    
+    M5.IMU.getAccelData(&accX, &accY, &accZ); // 加速度データ取得
+
+    diffAccX = accX - baseAccX; // 補正後の数値を表示値としてセット
+    diffAccY = accY - baseAccY;
+    diffAccZ = accZ - baseAccZ;
+
+    if(maxAccX < diffAccX){ maxAccX = diffAccX; }
+    if(maxAccY < diffAccY){ maxAccY = diffAccY; }
+    if(maxAccZ < diffAccZ){ maxAccZ = diffAccZ; }
+
+    spriteBase.fillScreen(BLACK);             // 画面の塗りつぶし
+
+    spriteBase.setTextColor(ORANGE, BLACK); // X軸測定値
+    spriteBase.setCursor(30, 24);
+    spriteBase.printf("X:%7.3f ", accX);    // 現在値
+    spriteBase.setCursor(140, 24);
+    spriteBase.printf("X:%7.3f ", maxAccX); // 最大値
+
+    spriteBase.setTextColor(CYAN, BLACK);   // Y軸測定値
+    spriteBase.setCursor(30, 44);
+    spriteBase.printf("Y:%7.3f ", accY);    // 現在値
+    spriteBase.setCursor(140, 44);
+    spriteBase.printf("Y:%7.3f ", maxAccY); // 最大値
+
+    spriteBase.setTextColor(GREEN, BLACK);  // Z軸測定値
+    spriteBase.setCursor(30, 64);
+    spriteBase.printf("Z:%7.3f ", accZ);    // 現在値
+    spriteBase.setCursor(140, 64);
+    spriteBase.printf("Z:%7.3f ", maxAccZ); // 最大値    
+
+    spriteBase.pushSprite(&lcd, 0, 0);
+  }
+
+  if (diffAccY > 0.5)
+  {
+    state = WAIT;
+  }
+  
+}
+
+void zeroReset()
+{
+  M5.IMU.getAccelData(&accX, &accY, &accZ); // 加速度データ取得
+  baseAccX = accX;                               // 取得値を補正値としてセット
+  baseAccY = accY;
+  baseAccZ = accZ;
+}
+
+void check_acc()
+{
+  zeroReset();
+  M5.update();                              // ボタン状態更新
+  M5.IMU.getAccelData(&accX, &accY, &accZ); // 加速度データ取得
 }
